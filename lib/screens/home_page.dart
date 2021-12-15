fe206042204f5c3a5528384bf82a6e1266985821
SNazarko <sorokinnazarne@gmail.com>
2021-12-15 13:27:39 +0200

    homePage 1/2

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:memory_box/screens/screens_element/appbar_clipper.dart';
 import '../constants.dart';
@@ -7,60 +8,161 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Column(
-        children: [
-          ClipPath(
-            clipper: AppbarClipper(),
-            child: Stack(
-              children: [
-                Container(
-                  color: kColorAppbar,
-                  width: double.infinity,
-                  height: 300,
-                ),
-                Positioned(
-                  left: 10,
-                  top: 30,
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.menu,
-                      color: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              child: Stack(
+                children: [
+                  Container(
+                    height: 380,
+                  ),
+                  ClipPath(
+                    clipper: AppbarClipper(),
+                    child: Container(
+                      color: kColorAppbar,
+                      width: double.infinity,
+                      height: 300,
                     ),
                   ),
-                ),
-                Positioned(
-                  top: 80,
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Row(
-                      children: const [
-                        Text(
-                          'Подборки',
-                          style: TextStyle(
-                            fontSize: 24,
-                            color: Colors.white,
-                          ),
-                        ),
-                        SizedBox(
-                          width: 120,
-                        ),
-                        Text(
-                          'Открыть все',
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
+                  Positioned(
+                    left: 0,
+                    top: 30,
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.menu,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
-                )
-              ],
+                  Positioned(
+                    top: 76,
+                    child: Container(
+                      width: screenWidth,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 16),
+                            child: Text(
+                              'Подборки',
+                              style: TextStyle(
+                                fontSize: 24,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 16),
+                            child: Text(
+                              'Открыть все',
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 110,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Container(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Padding(
+                              padding: EdgeInsets.all(10.0),
+                              child: Text(
+                                'Здесь будет твой набор сказок',
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.white),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                            const SizedBox(height: 40),
+                            TextButton(
+                              onPressed: () {},
+                              child: const Text(
+                                'Добавить',
+                                style: TextStyle(
+                                  decoration: TextDecoration.underline,
+                                  fontSize: 14,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                        width: screenWidth / 2.3,
+                        height: 240,
+                        decoration: const BoxDecoration(
+                            color: Color(0xD971A59F),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(15),
+                            )),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 110,
+                    right: 0,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 16, right: 16),
+                      child: Container(
+                        child: const Center(
+                          child: Text(
+                            'Тут',
+                            style: TextStyle(fontSize: 20, color: Colors.white),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        width: screenWidth / 2.3,
+                        height: 110,
+                        decoration: const BoxDecoration(
+                            color: Color(0xD9F1B488),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(15),
+                            )),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 230,
+                    right: 0,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 16, top: 24),
+                      child: Container(
+                        child: const Center(
+                          child: Text(
+                            'И тут',
+                            style: TextStyle(fontSize: 20, color: Colors.white),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        width: screenWidth / 2.3,
+                        height: 110,
+                        decoration: const BoxDecoration(
+                            color: Color(0xD9678BD2),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(15),
+                            )),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          )
-        ],
+            Container(),
+            Container()
+          ],
+        ),
       ),
     );
   }

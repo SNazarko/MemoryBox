8c7582a03a602b34c0f81ed0d2acbfb7396f3385
SNazarko <sorokinnazarne@gmail.com>
2021-12-20 22:54:35 +0200

    playPage 80%

--- lib/players/sound_record.dart
+++ lib/players/sound_record.dart
@@ -1,45 +1,188 @@
-import 'dart:html';
+import 'dart:async';
+import 'dart:io';
 
+import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:flutter_sound_lite/flutter_sound.dart';
+import 'package:flutter_sound_lite/public/flutter_sound_recorder.dart';
+import 'package:intl/date_symbol_data_file.dart';
+import 'package:intl/intl.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:path/path.dart' as path;
+import 'package:intl/intl.dart' show DateFormat;
 
 class SoundRecord {
-  FlutterSoundRecorder? _audioRecorder;
-  final pathToSaveAudio = 'audio_example.aac';
-  bool _isRecorderInitialised = false;
-  bool get isRecording => _audioRecorder!.isRecording;
+  final audioPlayer = AssetsAudioPlayer();
+  late String filePath;
+  late FlutterSoundRecorder _myRecorder;
+  // bool _play = false;
+  String recorderTxt = '00:00:00';
 
-  Future init() async {
-    _audioRecorder = FlutterSoundRecorder();
-    final status = await Permission.microphone.request();
-    if (status != PermissionStatus.granted) {
-      throw RecordingPermissionException('Microphone permission');
+  void startIt() async {
+    filePath = '/sdcard/Download/temp.wav';
+    _myRecorder = FlutterSoundRecorder();
+
+    await _myRecorder.openAudioSession(
+        focus: AudioFocus.requestFocusAndStopOthers,
+        category: SessionCategory.playAndRecord,
+        mode: SessionMode.modeDefault,
+        device: AudioDevice.speaker);
+    await _myRecorder.setSubscriptionDuration(const Duration(milliseconds: 10));
+    // await initializeDateFormatting();
+
+    await Permission.microphone.request();
+    await Permission.storage.request();
+    await Permission.manageExternalStorage.request();
+  }
+
+  Future<void> record() async {
+    Directory dir = Directory(path.dirname(filePath));
+    if (!dir.existsSync()) {
+      dir.createSync();
     }
-    _audioRecorder!.openAudioSession();
-    _isRecorderInitialised = true;
+    _myRecorder.openAudioSession();
+    await _myRecorder.startRecorder(
+      toFile: filePath,
+      codec: Codec.pcm16WAV,
+    );
+
+    StreamSubscription _recorderSubscription =
+        _myRecorder.onProgress!.listen((e) {
+      var date = DateTime.fromMillisecondsSinceEpoch(e.duration.inMilliseconds,
+          isUtc: true);
+      var txt = DateFormat('mm:ss:SS', 'en_GB').format(date);
+      recorderTxt = txt.substring(0, 8);
+    });
+    _recorderSubscription.cancel();
   }
 
-  Future dispose() async {
-    if (!_isRecorderInitialised) return;
-    _audioRecorder!.closeAudioSession();
-    _audioRecorder = null;
-    _isRecorderInitialised = false;
+  Future<String?> stopRecord() async {
+    _myRecorder.closeAudioSession();
+    return await _myRecorder.stopRecorder();
   }
 
-  Future _record() async {
-    if (!_isRecorderInitialised) return;
-    await _audioRecorder!.startRecorder(toFile: pathToSaveAudio);
+  Future<void> startPlaying() async {
+    audioPlayer.open(
+      Audio.file(filePath),
+      autoStart: true,
+      showNotification: true,
+    );
   }
 
-  Future _stop() async {
-    if (!_isRecorderInitialised) return;
-    await _audioRecorder!.stopRecorder();
-  }
-
-  Future toggleRecorder() async {
-    if (_audioRecorder!.isStopped) {
-      await _record();
-    } else {
-      await _stop();
-    }
+  Future<void> stopPlaying() async {
+    audioPlayer.stop();
   }
 }
+
+// import 'dart:async';
+//
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter_sound_lite/flutter_sound.dart';
+// import 'package:permission_handler/permission_handler.dart';
+//
+// class SoundRecord {
+//   FlutterSoundRecorder? _audioRecorder;
+//   final pathToSaveAudio = 'audio_example.aac';
+//   bool _isRecorderInitialised = false;
+//   bool get isRecording => _audioRecorder!.isRecording;
+//
+//   Future init() async {
+//     _audioRecorder = FlutterSoundRecorder();
+//     final status = await Permission.microphone.request();
+//     if (status != PermissionStatus.granted) {
+//       throw RecordingPermissionException('Microphone permission');
+//     }
+//     _audioRecorder!.openAudioSession();
+//     _isRecorderInitialised = true;
+//   }
+//
+//   Future dispose() async {
+//     if (!_isRecorderInitialised) return;
+//     _audioRecorder!.closeAudioSession();
+//     _audioRecorder = null;
+//     _isRecorderInitialised = false;
+//   }
+//
+//   Future _record() async {
+//     if (!_isRecorderInitialised) return;
+//     await _audioRecorder!.startRecorder(toFile: pathToSaveAudio);
+//   }
+//
+//   Future _stop() async {
+//     if (!_isRecorderInitialised) return;
+//     await _audioRecorder!.stopRecorder();
+//   }
+//
+//   Future toggleRecorder() async {
+//     if (_audioRecorder!.isStopped) {
+//       await _record();
+//     } else {
+//       await _stop();
+//     }
+//   }
+// }
+//
+// class TimerWidget extends StatefulWidget {
+//   final TimerController timerController;
+//   const TimerWidget({Key? key, required this.timerController})
+//       : super(key: key);
+//
+//   @override
+//   _TimerWidgetState createState() => _TimerWidgetState();
+// }
+//
+// class _TimerWidgetState extends State<TimerWidget> {
+//   Duration duration = Duration();
+//   Timer? timer;
+//   @override
+//   void initState() {
+//     widget.timerController.addListener(() {
+//       if (widget.timerController.value) {
+//         startTimer();
+//       } else {
+//         stopTimer();
+//       }
+//     });
+//     super.initState();
+//   }
+//
+//   void reset() => setState(() => duration = Duration());
+//
+//   void addTime() {
+//     final addSecond = 1;
+//     setState(() {
+//       final second = duration.inSeconds + addSecond;
+//       if (second < 0) {
+//         timer?.cancel();
+//       } else {
+//         duration = Duration(seconds: second);
+//       }
+//     });
+//   }
+//
+//   void startTimer({bool resets = true}) {
+//     if (!mounted) return;
+//     if (resets) {
+//       reset();
+//     }
+//     timer = Timer.periodic(Duration(seconds: 1), (_) => addTime());
+//   }
+//
+//   void stopTimer({bool resets = true}) {
+//     if (!mounted) return;
+//     if (resets) {
+//       reset();
+//     }
+//     setState(() => timer?.cancel());
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) => Center(
+//       // child: bildTime(),
+//       );
+// }
+//
+// class TimerController extends ValueNotifier<bool> {
+//   TimerController({bool isPlaying = false}) : super(isPlaying);
+//   void startTimer() => value = true;
+//   void stopTimer() => value = false;
+// }
--- lib/screens/play_page.dart
+++ lib/screens/play_page.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:memory_box/players/sound_record.dart';
 import 'package:memory_box/screens/screens_element/appbar_menu.dart';
 import 'package:memory_box/screens/screens_element/bottom_nav_bar.dart';
@@ -14,25 +17,32 @@ class PlayPage extends StatefulWidget {
 }
 
 class _PlayPageState extends State<PlayPage> {
-  final recorder = SoundRecord();
+  SoundRecord soundRecord = SoundRecord();
+  bool _isVisible = false;
+  bool _play = false;
+  // final timerController = TimerController();
+  // final recorder = SoundRecord();
   @override
   void initState() {
-    // TODO: implement initState
+    soundRecord.startIt();
     super.initState();
-    recorder.init();
+    // recorder.init();
   }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-    recorder.dispose();
-  }
+  //
+  // @override
+  // void dispose() {
+  //   // TODO: implement dispose
+  //   super.dispose();
+  //   recorder.dispose();
+  // }
 
   @override
   Widget build(BuildContext context) {
-    final isRecording = recorder.isRecording;
-    final icon = isRecording ? 'stop' : 'play';
+    // final isRecording = recorder.isRecording;
+    // Widget icon = isRecording
+    //     ? const Icon(Icons.pause_circle_filled)
+    //     : Icon(Icons.play_circle_filled);
+    // 'Icons.pause_circle_filled' : 'Icons.play_circle_filled';
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -49,67 +59,174 @@ class _PlayPageState extends State<PlayPage> {
                   decoration: kBorderContainer2,
                   child: Column(
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 20),
-                        child: Row(
-                          children: [
-                            IconButton(
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: Visibility(
+                          visible: !_isVisible,
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 20),
+                            child: TextButton(
                               onPressed: () {},
-                              icon: Image.asset('images/rec_upload.png'),
-                              padding: EdgeInsets.symmetric(horizontal: 15),
+                              child: const Text(
+                                'Отменить',
+                                style: TextStyle(fontSize: 16),
+                              ),
                             ),
-                            IconButton(
+                          ),
+                        ),
+                      ),
+                      Visibility(
+                        visible: _isVisible,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 10, horizontal: 20),
+                          child: Row(
+                            children: [
+                              IconButton(
                                 onPressed: () {},
-                                icon: Image.asset(
-                                    'images/rec_paper_download.png'),
-                                padding: EdgeInsets.symmetric(horizontal: 15)),
-                            IconButton(
-                                onPressed: () {},
-                                icon: Image.asset('images/rec_delete.png'),
-                                padding: EdgeInsets.symmetric(horizontal: 15)),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 50),
-                              child: TextButton(
+                                icon: Image.asset('images/rec_upload.png'),
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 15),
+                              ),
+                              IconButton(
+                                  onPressed: () {},
+                                  icon: Image.asset(
+                                      'images/rec_paper_download.png'),
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 15)),
+                              IconButton(
+                                  onPressed: () {},
+                                  icon: Image.asset('images/rec_delete.png'),
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 15)),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 50),
+                                child: TextButton(
                                   onPressed: () {},
                                   child: const Text(
                                     'Сохранить',
-                                    style: TextStyle(fontSize: 16),
-                                  )),
-                            )
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Visibility(
+                        visible: !_isVisible,
+                        child: const Text(
+                          'Запись',
+                          style: TextStyle(
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 60,
+                      ),
+                      Visibility(
+                        visible: _isVisible,
+                        child: const Text(
+                          'Аудиозапись 1',
+                          style: TextStyle(
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
+                      // TimerWidget(
+                      //   timerController: TimerController(),
+                      // ),
+                      const SizedBox(
+                        height: 140,
+                      ),
+                      Visibility(
+                        visible: !_isVisible,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: 10,
+                              height: 10,
+                              decoration: const BoxDecoration(
+                                color: Color(0xFFE27777),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(20),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 5,
+                            ),
+                            Text(
+                              soundRecord.recorderTxt,
+                              style: const TextStyle(
+                                fontSize: 18,
+                              ),
+                            ),
                           ],
                         ),
                       ),
                       const SizedBox(
-                        height: 80,
+                        height: 40,
                       ),
-                      Text(
-                        'Аудиозапись 1',
-                        style: TextStyle(fontSize: 24),
-                      ),
-                      SizedBox(
-                        height: 200,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          IconButton(
-                              onPressed: () {}, icon: Icon(Icons.replay_10)),
-                          IconButton(
-                              splashRadius: 110,
+                      Visibility(
+                        visible: _isVisible,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.replay_10,
+                              ),
+                            ),
+                            IconButton(
                               iconSize: 100,
                               onPressed: () async {
-                                final isRecording =
-                                    await recorder.toggleRecorder();
-                                setState(() {});
+                                setState(() {
+                                  _play = !_play;
+                                });
+                                if (_play) await soundRecord.startPlaying();
+                                if (!_play) await soundRecord.stopPlaying();
+                                // final isRecording =
+                                //     await recorder.toggleRecorder();
+                                // setState(() {});
                               },
-                              icon: Image.asset(
-                                'images/$icon.png',
-                              )),
-                          IconButton(
-                              onPressed: () {}, icon: Icon(Icons.forward_10)),
-                        ],
-                      )
+                              icon: _play
+                                  ? const Icon(Icons.play_circle_filled)
+                                  : const Icon(Icons.pause_circle_filled),
+                            ),
+                            IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.forward_10)),
+                          ],
+                        ),
+                      ),
+                      Visibility(
+                          visible: !_isVisible,
+                          child: IconButton(
+                            iconSize: 100,
+                            onPressed: () async {
+                              setState(() {
+                                _play = !_play;
+                              });
+                              if (_play) await soundRecord.record();
+                              if (!_play) {
+                                await soundRecord.stopRecord();
+                                _isVisible = !_isVisible;
+                              }
+                            },
+                            icon: _play
+                                ? const Icon(Icons.play_circle_filled)
+                                : const Icon(Icons.pause_circle_filled),
+                          )),
                     ],
                   ),
                 ),

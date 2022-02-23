8b367aba3bfe94ed5acdee77f3f382f11c2ebb93
SNazarko <sorokinnazarne@gmail.com>
2022-02-23 19:30:52 +0200

    add delete collections 50%

--- lib/pages/collections_pages/collections/widgets/collections_item.dart
+++ lib/pages/collections_pages/collections/widgets/collections_item.dart
@@ -6,7 +6,7 @@ import 'package:memory_box/resources/app_colors.dart';
 import 'package:provider/provider.dart';
 
 class CollectionsItem extends StatelessWidget {
-  const CollectionsItem(
+  CollectionsItem(
       {Key? key,
       this.title,
       this.quality,
@@ -19,6 +19,7 @@ class CollectionsItem extends StatelessWidget {
   final String? quality;
   final String? image;
   final String? data;
+  final bool done = false;
 
   @override
   Widget build(BuildContext context) {
@@ -26,77 +27,185 @@ class CollectionsItem extends StatelessWidget {
       borderRadius: const BorderRadius.all(
         Radius.circular(20.0),
       ),
-      child: GestureDetector(
-        onTap: () {
-          context.read<CollectionsItemPageModel>().setTitle(title!);
-          context.read<CollectionsItemPageModel>().setSubTitle(subTitle!);
-          context.read<CollectionsItemPageModel>().setPhoto(image!);
-          context.read<CollectionsItemPageModel>().setData(data!);
-          context.read<CollectionsItemPageModel>().setQuality(quality!);
-          Navigator.pushNamed(
-            context,
-            CollectionsItemPage.routeName,
-          );
-        },
-        child: SizedBox(
-          width: 185.0,
-          height: 250.0,
-          child: Stack(
-            children: [
-              image != ''
-                  ? Image.network(
-                      image!,
-                      fit: BoxFit.fill,
-                      width: 185.0,
-                      height: 250.0,
-                    )
-                  : const ColoredBox(color: Colors.cyan),
-              Padding(
-                padding: const EdgeInsets.only(left: 10, right: 5, bottom: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Expanded(
-                      child: Text(
-                        title!,
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 3,
-                        style: const TextStyle(
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.w700,
-                          color: AppColor.white100,
-                        ),
+      child: getCollectionDone(context),
+    );
+  }
+
+  getCollectionItem(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        context.read<CollectionsItemPageModel>().setTitle(title!);
+        context.read<CollectionsItemPageModel>().setSubTitle(subTitle!);
+        context.read<CollectionsItemPageModel>().setPhoto(image!);
+        context.read<CollectionsItemPageModel>().setData(data!);
+        context.read<CollectionsItemPageModel>().setQuality(quality!);
+        Navigator.pushNamed(
+          context,
+          CollectionsItemPage.routeName,
+        );
+      },
+      child: SizedBox(
+        width: 185.0,
+        height: 250.0,
+        child: Stack(
+          children: [
+            image != ''
+                ? Image.network(
+                    image!,
+                    fit: BoxFit.fill,
+                    width: 185.0,
+                    height: 250.0,
+                  )
+                : const ColoredBox(color: Colors.cyan),
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 5, bottom: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: Text(
+                      title!,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 3,
+                      style: const TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w700,
+                        color: AppColor.white100,
                       ),
                     ),
-                    Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Text(
-                            '$quality аудио',
-                            style: const TextStyle(
-                              fontSize: 14.0,
-                              fontWeight: FontWeight.w400,
-                              color: AppColor.white100,
-                            ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          '$quality аудио',
+                          style: const TextStyle(
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.w400,
+                            color: AppColor.white100,
                           ),
-                          const Text(
-                            '3:33 часа',
-                            style: TextStyle(
-                              fontSize: 14.0,
-                              fontWeight: FontWeight.w400,
-                              color: AppColor.white100,
-                            ),
-                          )
-                        ],
-                      ),
-                    )
-                  ],
-                ),
+                        ),
+                        const Text(
+                          '3:33 часа',
+                          style: TextStyle(
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.w400,
+                            color: AppColor.white100,
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  getCollectionDone(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: SizedBox(
+        width: 185.0,
+        height: 250.0,
+        child: Stack(
+          children: [
+            image != ''
+                ? Image.network(
+                    image!,
+                    fit: BoxFit.fill,
+                    width: 185.0,
+                    height: 250.0,
+                  )
+                : const ColoredBox(color: Colors.cyan),
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 5, bottom: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: Text(
+                      title!,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 3,
+                      style: const TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w700,
+                        color: AppColor.white100,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          '$quality аудио',
+                          style: const TextStyle(
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.w400,
+                            color: AppColor.white100,
+                          ),
+                        ),
+                        const Text(
+                          '3:33 часа',
+                          style: TextStyle(
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.w400,
+                            color: AppColor.white100,
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              width: 185.0,
+              height: 250.0,
+              decoration: BoxDecoration(
+                color: Colors.black.withOpacity(0.4),
+                gradient: LinearGradient(
+                    colors: done
+                        ? [const Color(0xFF000000), const Color(0xFF000000)]
+                        : [const Color(0xFF000000), const Color(0xFF454545)],
+                    begin: Alignment.bottomRight),
+              ),
+            ),
+            Center(
+              child: Stack(
+                children: [
+                  Container(
+                    width: 50.0,
+                    height: 50.0,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: AppColor.white),
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(25.0),
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.done,
+                      color: done
+                          // context.watch<ModelPlayerMiniPodborki>().done
+                          ? AppColor.colorText
+                          : AppColor.white,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );

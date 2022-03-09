e5e37dca998b32f9f28392265acc052059041793
SNazarko <sorokinnazarne@gmail.com>
2022-03-09 11:49:45 +0200

    refactoring page CollectionAddPage

---
+++ lib/pages/collections_pages/collection_add_audio/widgets/appbar_header_collection_add_audio.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:memory_box/pages/search_page/search_page_model.dart';
+import 'package:memory_box/resources/app_colors.dart';
+import 'package:memory_box/resources/app_icons.dart';
+import 'package:memory_box/resources/constants.dart';
+import 'package:memory_box/widgets/appbar_clipper.dart';
+import 'package:memory_box/widgets/icon_back.dart';
+import 'package:provider/src/provider.dart';
+
+class AppbarHeaderCollectionAddAudio extends StatelessWidget {
+  const AppbarHeaderCollectionAddAudio({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(),
+        ClipPath(
+          clipper: AppbarClipper(),
+          child: Container(
+            color: AppColor.colorAppbar2,
+            width: double.infinity,
+            height: 220.0,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 15, top: 15),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconBack(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 20.0),
+                child: Text(
+                  'Выбрать',
+                  style: kTitleTextStyle2,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Padding(
+                  padding: EdgeInsets.only(top: 15),
+                  child: Text(
+                    'Добавить',
+                    style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w400,
+                        color: AppColor.white100),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        const _SearchPanel(),
+      ],
+    );
+  }
+}
+
+class _SearchPanel extends StatelessWidget {
+  const _SearchPanel({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    return Padding(
+      padding: const EdgeInsets.only(left: 20.0, top: 121.0, right: 12.0),
+      child: Container(
+        height: 60.0,
+        decoration: const BoxDecoration(
+          color: AppColor.white,
+          borderRadius: BorderRadius.all(Radius.circular(30.0)),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 29),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(
+                  width: screenWidth * 0.65,
+                  child: TextField(
+                    onChanged: (searchTxt) {
+                      var text = searchTxt;
+                      context
+                          .read<SearchPageModel>()
+                          .setSearchAddAudio(text.toLowerCase());
+                    },
+                    style: const TextStyle(
+                      fontSize: 20.0,
+                      color: AppColor.colorText,
+                    ),
+                    decoration: const InputDecoration(
+                      hintText: 'Поиск',
+                      hintStyle: TextStyle(
+                        fontSize: 20.0,
+                        color: AppColor.colorText50,
+                      ),
+                      border: OutlineInputBorder(borderSide: BorderSide.none),
+                    ),
+                  )),
+              GestureDetector(
+                onTap: () {},
+                child: Image.asset(
+                  AppIcons.search,
+                  color: AppColor.colorText,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/collections_pages/collection_add_audio/widgets/list_players_collection_add_audio.dart
@@ -0,0 +1,103 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:memory_box/models/audio_model.dart';
+import 'package:memory_box/pages/collections_pages/collection_edit/collection_edit_model.dart';
+import 'package:memory_box/pages/search_page/search_page_model.dart';
+import 'package:memory_box/repositories/audio_repositories.dart';
+import 'package:memory_box/widgets/player_mini_podborki.dart';
+import 'package:provider/provider.dart';
+
+class ListPlayersCollectionAddAudio extends StatelessWidget {
+  ListPlayersCollectionAddAudio({Key? key}) : super(key: key);
+  final AudioRepositories repositories = AudioRepositories();
+
+  Stream<List<AudioModel>> readAudioCollectionEdit(BuildContext context) =>
+      FirebaseFirestore.instance
+          .collection('CollectionsTale')
+          .doc(Provider.of<CollectionsEditModel>(context, listen: false)
+              .getTitle)
+          .collection('Audio')
+          .where('searchName',
+              arrayContains: context.watch<SearchPageModel>().getSearchAddAudio)
+          .snapshots()
+          .map((snapshot) => snapshot.docs
+              .map((doc) => AudioModel.fromJson(doc.data()))
+              .toList());
+
+  Widget buildAudio(AudioModel audio) => PlayerMiniPodborki(
+        duration: '${audio.duration}',
+        url: '${audio.audioUrl}',
+        name: '${audio.audioName}',
+        done: audio.done!,
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    final state = context.watch<SearchPageModel>().getSearchAddAudio;
+    final double screenHeight = MediaQuery.of(context).size.height;
+    if (state == '') {
+      return Column(
+        children: [
+          SizedBox(
+            height: screenHeight * 0.95,
+            child: StreamBuilder<List<AudioModel>>(
+              stream: repositories.readAudioCollectionEdit(
+                  Provider.of<CollectionsEditModel>(context, listen: false)
+                      .getTitle),
+              builder: (
+                context,
+                snapshot,
+              ) {
+                if (snapshot.hasError) {
+                  return const Text('Ошибка');
+                }
+                if (snapshot.hasData) {
+                  final audio = snapshot.data!;
+                  return ListView(
+                    padding: const EdgeInsets.only(top: 230, bottom: 40),
+                    children: audio.map(buildAudio).toList(),
+                  );
+                } else {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              },
+            ),
+          ),
+        ],
+      );
+    } else {
+      return Column(
+        children: [
+          Container(
+            height: screenHeight * 0.95,
+            child: StreamBuilder<List<AudioModel>>(
+              stream: readAudioCollectionEdit(context),
+              builder: (
+                context,
+                snapshot,
+              ) {
+                if (snapshot.hasError) {
+                  return const Text('Ошибка');
+                }
+                if (snapshot.hasData) {
+                  final audio = snapshot.data!;
+                  return ListView(
+                    padding: const EdgeInsets.only(top: 230, bottom: 40),
+                    children: audio.map(buildAudio).toList(),
+                  );
+                } else {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              },
+            ),
+          ),
+        ],
+      );
+    }
+  }
+}

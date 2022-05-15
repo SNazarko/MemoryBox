5249edec765e1d60e078ce0cae6acc5b99a80f26
SNazarko <sorokinnazarne@gmail.com>
2022-05-15 10:27:03 +0300

    added icon App

--- lib/pages/support_message_page/bloc/support_message/support_message_bloc.dart
+++ lib/pages/support_message_page/bloc/support_message/support_message_bloc.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/cupertino.dart';
 
